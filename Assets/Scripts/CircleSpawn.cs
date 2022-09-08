13f8583d2cfac19e99954b785b566191dff9bbd7
Akshay <akshaybharadhwaj98@gmail.com>
2022-09-07 22:19:27 -0400

    Updated Circle Spawn Script

--- Assets/Scripts/CircleSpawn.cs
+++ Assets/Scripts/CircleSpawn.cs
@@ -15,7 +15,19 @@ public class CircleSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform.tag == "Target Sprite")
+                {
+                    Debug.Log("Sprite Clicked");
+                }
+            }
+        }
     }
 
     void Spawn()
