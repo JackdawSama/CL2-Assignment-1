103f3a8f96f39a0c91884b735de314cc4c1866ec
Akshay <akshaybharadhwaj98@gmail.com>
2022-09-08 20:29:37 -0400

    Added raycasts and delet object to make it gamery

--- Assets/Scripts/CircleSpawn.cs
+++ Assets/Scripts/CircleSpawn.cs
@@ -6,34 +6,43 @@ public class CircleSpawn : MonoBehaviour
 {
 
     public Sprite[] sprites;
+    private int score = 0;
+    private int num;
+    private Vector2 pos;
+    public int radius;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawn", 1, 1);
+        InvokeRepeating("Spawn", 0.3f, Random.Range(1,3));                                      //spawns a sprite randomly every 1 to 2 seconds
     }
 
     // Update is called once per frame
     void Update()
     {
+        pos = new Vector2(Random.Range(-radius, radius), Random.Range(-radius, radius));        //randomises the position
+
+        //checks if the left mouse button is clicked and if true it executes a raycasts
         if(Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            Vector2 ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray, Vector2.zero);
 
-            if(Physics.Raycast(ray, out hit))
+            //checks if the raycast collides with an object and if true deletes the game object and increases the player score
+            if(hit.collider != null)
             {
-                if(hit.transform.tag == "Target Sprite")
-                {
-                    Debug.Log("Sprite Clicked");
-                }
+                Destroy(hit.collider.gameObject);
+                score ++;
+                Debug.Log("Sprite Clicked");
+                Debug.Log("Score: " + score);
             }
         }
     }
 
+    //function to spwan a game object
     void Spawn()
     {
-        GameObject go = Instantiate(Resources.Load("Prefabs/Blue")) as GameObject;
-        int num = GetComponent<ColourPicker>().SetSprite();
-        go.GetComponent<SpriteRenderer>().sprite = sprites[num];
+        GameObject go = Instantiate(Resources.Load("Prefabs/Blue"), pos, Quaternion.identity) as GameObject;    //creates an instance of the seleced object from the resources folder as a game object
+        num = GetComponent<ColourPicker>().SetSprite();                                                         //gets a random number from the ColourPicker script's SetSprite() funcion
+        go.GetComponent<SpriteRenderer>().sprite = sprites[num];                                                //sets the sprite from he list of sprites available based on the return of the SetSprite() function
     }
 }
--- Assets/Scripts/ColourPicker.cs
+++ Assets/Scripts/ColourPicker.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ColourPicker : MonoBehaviour
 {
+    //function to randomise and obtain an integer value that ranges from 0 to 3
     public int SetSprite()
     {
         return Random.Range(0, 4);
